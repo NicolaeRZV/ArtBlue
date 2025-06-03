f9cabe23ec9b98bde11db2600564202fae89ca83
RazvanNicolae <122481441+NicolaeRZV@users.noreply.github.com>
2025-06-03 10:07:44 +0300

    Add files via upload

---
+++ ArtBlue/script.js
@@ -0,0 +1,38 @@
+function openArticles(){
+  window.location.href = "index.html";
+
+}
+function openReviews(){
+    window.location.href = "reviews.html";
+  
+  }
+  function openContact(){
+    window.location.href = "Contact.html";
+  
+  }
+  function openPacket1(){
+    window.location.href = "Packet1.html";
+  
+  }  
+
+   
+  function openPacket2(){
+    window.location.href = "Packet2.html";
+  
+  }  
+
+  
+  function openPacket3(){
+    window.location.href = "Packet3.html";
+  
+  }  
+  function myFunction() {
+  var x = document.getElementById("Demo");
+  if (x.className.indexOf("w3-show") == -1) { 
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+  
